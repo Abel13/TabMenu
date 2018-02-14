1cb95555d6416ed8e26ff0d57347d97ae1cc9290
Abel <abel.o.d@outlook.com>
2018-02-14 20:21:12 -0200

    First Commit

---
+++ TabMenu/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TabMenu
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+
+            GridCursor.Margin = new Thickness(10 + (150 * index), 0, 0, 0);
+
+            switch(index)
+            {
+                case 0:
+                    GridMain.Background = Brushes.Aquamarine;
+                    break;
+                case 1:
+                    GridMain.Background = Brushes.Beige;
+                    break;
+                case 2:
+                    GridMain.Background = Brushes.CadetBlue;
+                    break;
+                case 3:
+                    GridMain.Background = Brushes.DarkBlue;
+                    break;
+                case 4:
+                    GridMain.Background = Brushes.Firebrick;
+                    break;
+                case 5:
+                    GridMain.Background = Brushes.Gainsboro;
+                    break;
+                case 6:
+                    GridMain.Background = Brushes.HotPink;
+                    break;
+            }
+        }
+    }
+}
